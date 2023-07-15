eee11c653e193b77d412f18e3a801f51703c2322
Emily McMahon <emilyemcmahonn@gmail.om>
2023-07-15 12:39:25 -0700

    Pass vs Fail

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('\u2705 \u2705 \u2705 Assertion Passed: ' + actual + ' === ' + expected); // Used the Unicode representation for the green check
+  } else {
+    console.log('\u274C \u274C \u274C Assertion Failed: ' + actual + ' !== ' + expected); // Used the Unicode representation for the red x
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
