9c191c08c71c1df2b808f515b835b4394a529ec2
Emily McMahon <emilyemcmahonn@gmail.om>
2023-07-15 12:46:16 -0700

    Changed it to template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,20 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log('\u2705 \u2705 \u2705 Assertion Passed: ' + actual + ' === ' + expected); // Used the Unicode representation for the green check
+    console.log(`\u2705 \u2705 \u2705 Assertion Passed: ${actual} === ${expected}`); // Used the Unicode representation for the green check
   } else {
-    console.log('\u274C \u274C \u274C Assertion Failed: ' + actual + ' !== ' + expected); // Used the Unicode representation for the red x
+    console.log(`\u274C \u274C \u274C Assertion Failed: ${actual} !== ${expected}`); // Used the Unicode representation for the red x
   }
 };
+
+// OLD WAY WITHOUT TEMPLATE LITERALS 
+//const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log('\u2705 \u2705 \u2705 Assertion Passed: ' + actual + ' === ' + expected); // Used the Unicode representation for the green check
+//   } else {
+//     console.log('\u274C \u274C \u274C Assertion Failed: ' + actual + ' !== ' + expected); // Used the Unicode representation for the red x
+//   }
+// };
 
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
