242f25260efca8daff781a8e75c87eb7f2190fd0
Emily McMahon <emilyemcmahonn@gmail.om>
2023-07-16 22:50:35 -0700

    Printing the head in an array

---
+++ head.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u2705 \u2705 \u2705 Assertion Passed: ${actual} === ${expected}`); // Used the Unicode representation for the green check
+  } else {
+    console.log(`\u274C \u274C \u274C Assertion Failed: ${actual} !== ${expected}`); // Used the Unicode representation for the red x
+  }
+};
+
+function head(array) {
+  return array[0];
+}
+
+// Test Assertions 
+assertEqual(head([5,6,7]), 5);
+assertEqual(head([4]));
+assertEqual(head([]));
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
