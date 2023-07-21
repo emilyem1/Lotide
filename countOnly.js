80514e9f567e8e5b68829fcbf7573834a595820e
Emily McMahon <emilyemcmahonn@gmail.om>
2023-07-21 14:50:56 -0700

    function that counts how many matches there are between object and array

--- countOnly.js
+++ countOnly.js
@@ -20,27 +20,27 @@ const assertEqual = function(actual, expected) {
 * through the object to go through all the names
 * 3) If itemsToCount has one of those names, and it's true (if it's false it
 * will skip it anyway, so no need to define allItems[name] === false condition)
-* 4) If matchTotal already has the name +1
+* 4) If results already has the name +1
 * 5) Else give a value of 1
-* 6) Return matchTotal which will either have a value or continue being
+* 6) Return results which will either have a value or continue being
 * undefined
 */
 
 // countOnly function !!!
 
 const countOnly = function(allItems, itemsToCount) {
-  matchTotal = {}; // Empty object to push into
+  let results = {}; // Empty object to push into
 
   for (const name of allItems) { /* for of loop. 'const name' gives a name to the elements inside the object partyList to be called on */
     if (itemsToCount[name]) { /* if itemsToCount has one of those names and it's true */
-      if (matchTotal[name]) { // if match total
-        matchTotal[name] += 1; // already has the name, +1
+      if (results[name]) { // if match total
+        results[name] += 1; // already has the name, +1
       } else {
-        matchTotal[name] = 1; // else it gets a value of 1
+        results[name] = 1; // else it gets a value of 1
       }
     }
   }
-  return matchTotal; // return the object that now stores the values
+  return results; // return the object that now stores the values
 };
 
 // Test
@@ -56,14 +56,14 @@ const firstNames = [
   "Joe"
 ];
 
-const partyList = {
+const results = {
   Jason: true,
   Karima: true,
   Fang: true,
   Agouhanna: false };
 
 
-const result1 = countOnly(firstNames, partyList);
+const result1 = countOnly(firstNames, results);
 
 assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
