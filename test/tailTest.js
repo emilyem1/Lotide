45e288c69c9da21483183bbe7cca19258a1aaeeb
Emily McMahon <emilyemcmahonn@gmail.om>
2023-08-04 14:26:12 -0700

    test for tail function

---
+++ test/tailTest.js
@@ -0,0 +1,15 @@
+const assertEqual = require('../assertEqual');
+const tail = require('../tail.js');
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+const test = tail([4]);
+assertEqual(test.length, 1); // ensuring that after the slice there is nothing left in array
+assertEqual(test[0], 4);
