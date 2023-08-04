b4eab83f28ce7708190d661e1497237eafcdd4b7
Emily McMahon <emilyemcmahonn@gmail.om>
2023-08-04 14:26:53 -0700

    dried the wet code

--- tail.js
+++ tail.js
@@ -1,28 +1,7 @@
-// AssertEqual function
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`\u2705 \u2705 \u2705 Assertion Passed: ${actual} === ${expected}`); // Used the Unicode representation for the green check
-  } else {
-    console.log(`\u274C \u274C \u274C Assertion Failed: ${actual} !== ${expected}`); // Used the Unicode representation for the red x
-  }
-};
 // Tail Function
 
 const tail = function(array) {
   return array.splice(1); // Starts array at 1, cuts [0]
 };
-// Test Case: Check the original array
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-const test = tail([4]);
-assertEqual(test.length, 1); // ensuring that after the slice there is nothing left in array
-assertEqual(test[0], 4);
+module.exports = tail;
