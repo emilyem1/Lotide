4257f976651bc16f1ead56dd4ebc8eba15911bc0
Emily McMahon <emilyemcmahonn@gmail.om>
2023-08-04 14:15:21 -0700

    test for function

--- test/headTest.js
+++ test/headTest.js
@@ -1,1 +1,8 @@
-djkfdljfd
+const assertEqual = require('../assertEqual');
+const head = require('../head');
+
+// Test Assertions 
+assertEqual(head([5,6,7]), 5); // Returns 5
+assertEqual(head([4])); // Returns 4
+assertEqual(head([])); // Returns undefined
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // Returns hello 
