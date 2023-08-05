c3e480c70bc6e318fb6d563ec53a68d462416cdf
Emily McMahon <emilyemcmahonn@gmail.om>
2023-08-04 22:25:55 -0700

    mocha chai test

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,24 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
 
-// Test Assertions 
-assertEqual(head([5,6,7]), 5); // Returns 5
-assertEqual(head([4])); // Returns 4
-assertEqual(head([])); // Returns undefined
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // Returns hello 
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("returns '4' for ['4']", () => {
+    assert.strictEqual(head(['4']), '4');
+  });
+
+  it("returns '[]' for '[]'", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  });
+});
