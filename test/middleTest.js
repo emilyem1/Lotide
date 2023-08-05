6fbc75c28926d90367e4459e4b3753ed41995460
Emily McMahon <emilyemcmahonn@gmail.om>
2023-08-04 22:45:43 -0700

    added mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,20 +1,20 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-// *** Test ****
+describe("#middle", () => {
+  it("For arrays with one or two elements, return an empty array.", () => {
+    assert.deepEqual(middle([1]), []);
+  });
 
-/*For arrays with one or two elements, there is no middle. Return an empty array. */
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
+  it("For arrays with odd number of elements, an array containing a single middle element should be returned.", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
 
-/*For arrays with odd number of elements, an array containing a single middle element should be returned. */
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+  it("For arrays with an even number of elements, an array containing the two elements in the middle should be returned", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
 
-/* For arrays with an even number of elements, an array containing the two elements in the middle should be returned */
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-
-// For arrays with strings
-assertArraysEqual(middle(['Hello', 'My', 'Name']), ['My']);
-assertArraysEqual(middle(['Hello', 'My', 'Name', 'Emily']), ['My', 'Name']);
+  it("For arrays with strings", () => {
+    assert.deepEqual(middle(['Hello', 'My', 'Name']), ['My']);
+  });
+});
