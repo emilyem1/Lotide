26b9cf59d6fbd0c22475ee86a4cf4384f1599a9f
Emily McMahon <emilyemcmahonn@gmail.om>
2023-08-04 23:06:20 -0700

    index of all of the lotide functions

---
+++ index.js
@@ -0,0 +1,38 @@
+// index.js
+const head = require('../lotide/head');
+const tail = require('../lotide/tail');
+const middle = require('../lotide/middle');
+const assertArraysEqual = require('../lotide/assertArraysEqual');
+const assertEqual = require('../lotide/assertEqual');
+const assertObjectsEqual = require('../lotide/assertObjectsEqual');
+const countLetters = require('../lotide/countLetters');
+const countOnly = require('../lotide/countOnly');
+const eqArrays = require('../lotide/eqArrays');
+const eqObjects = require('../lotide/eqObjects');
+const findKey = require('../lotide/findKey');
+const findKeyByValue = require('../lotide/findKeyByValue');
+const flatten = require('../lotide/flatten');
+const letterPositions = require('../lotide/letterPositions');
+const map = require('../lotide/map');
+const takeUntil = require('../lotide/takeUntil');
+const without = require('../lotide/without');
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  flatten: flatten,
+  letterPositions: letterPositions,
+  map: map,
+  takeUntil: takeUntil,
+  without: without
+};
