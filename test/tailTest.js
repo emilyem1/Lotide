bb5af48217ed00342cf09db693e0cd8919221b43
Emily McMahon <emilyemcmahonn@gmail.om>
2023-08-04 22:39:44 -0700

    added mocha chai to test

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,19 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail.js');
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
+const result = tail(['Hello', 'Lighthouse', 'Labs']);
 const test = tail([4]);
-assertEqual(test.length, 1); // ensuring that after the slice there is nothing left in array
-assertEqual(test[0], 4);
+
+describe("#tail", () => {
+  it("returns 'Lighthouse' for result[0]", () => {
+    assert.deepEqual(result[0], 'Lighthouse');
+  });
+
+  it("returns 'Labs' for result[1]", () => {
+    assert.deepEqual(result[1], 'Labs');
+  });
+
+  it("returns 'undefined' for test[0]", () => {
+    assert.deepEqual(test[0], undefined);
+  });
+});
