2aaf6bc7d1811e12b49b00467e1295ee4afbfe0e
Emily McMahon <emilyemcmahonn@gmail.om>
2023-08-10 14:01:56 -0700

    function using slice to return tail of array

--- tail.js
+++ tail.js
@@ -1,7 +1,10 @@
 // Tail Function
 
 const tail = function(array) {
-  return array.splice(1); // Starts array at 1, cuts [0]
+  return array.slice(1); // Starts array at 1, cuts [0]
 };
+
+// Originally had this with .splice which modifies the original array.
+// Now with .slice it returns a new array containing elements from [1]
 
 module.exports = tail;
