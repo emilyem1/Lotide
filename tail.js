a8e9b6c2ad5a745139d63230037a2e8b2a378129
Emily McMahon <emilyemcmahonn@gmail.om>
2023-09-12 16:14:57 -0700

    Removing unneeded comments

--- tail.js
+++ tail.js
@@ -4,8 +4,4 @@ const tail = function(array) {
   return array.slice(1); // Starts array at 1, cuts [0]
 };
 
-// Note for submission: 
-// Originally had this with .splice which modifies the original array.
-// Now with .slice it returns a new array containing elements from [1]
-
 module.exports = tail;
