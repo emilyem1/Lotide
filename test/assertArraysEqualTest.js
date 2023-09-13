7429dcd542f089762a27e31b84f162e3bf483812
Emily McMahon <101762106+emilyem1@users.noreply.github.com>
2023-09-12 17:10:13 -0700

    Removing previous attempt at mocha/chai test, instead using simple test'

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,33 +1,9 @@
 const assertArraysEqual = require('../assertArraysEqual');
 
-describe('assertArraysEqual', () => {
-  it('should return true when comparing [1, 2, 3] to [1, 2, 3]', () => {
-    if (assertArraysEqual([1, 2, 3], [1, 2, 3])) {
-      console.log("true");
-    }
-  });
+// Test for assertArraysEqual
 
-  it('should return false when comparing [1, 2, 3] to [1, 2, 4]', () => {
-    if (assertArraysEqual([1, 2, 3], [1, 2, 4])) {
-      console.log("false");
-    }
-  });
-
-  it('should return true when comparing ["a", "b", "c"] to ["a", "b", "c"]', () => {
-    if (assertArraysEqual(['a', 'b', 'c'], ['a', 'b', 'c'])) {
-      console.log("true");
-    }
-  });
-
-  it('should return false when comparing ["a", "b", "c"] to ["a", "b", "d"]', () => {
-    if (assertArraysEqual(['a', 'b', 'c'], ['a', 'b', 'd'])) {
-      console.log("false");
-    }
-  });
-
-  it('should return false when comparing [1, 2, 3] to ["1", "2", "3"]', () => {
-    if (assertArraysEqual([1, 2, 3], ['1', '2', '3'])) {
-      console.log("false");
-    }
-  });
-});
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // true
+assertArraysEqual([1, 2, 3], [1, 2, 4]); // false
+assertArraysEqual(['a', 'b', 'c'], ['a', 'b', 'c']); // true
+assertArraysEqual(['a', 'b', 'c'], ['a', 'b', 'd']); // false
+assertArraysEqual([1, 2, 3], ['1', '2', '3']); // false
