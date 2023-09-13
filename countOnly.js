6c41fce0a5983af2aa73179b93313af0f640355e
Emily McMahon <101762106+emilyem1@users.noreply.github.com>
2023-09-12 17:14:45 -0700

    Exporting countOnly function, moving test code into separate file

--- countOnly.js
+++ countOnly.js
@@ -1,14 +1,3 @@
-// **** SET UP ****
-
-// assertEqual function: tells if two elements are ===
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`\u2705 \u2705 \u2705 Assertion Passed: ${actual} === ${expected}`); // Used the Unicode representation for the green check
-  } else {
-    console.log(`\u274C \u274C \u274C Assertion Failed: ${actual} !== ${expected}`); // Used the Unicode representation for the red x
-  }
-};
-
 /* Goal: countOnly will be given an array and an object. It will return an object containing counts of everything that the input object listed. */
 
 /* Main question: How to make the function count how many matches there are between object and array */
@@ -43,29 +32,4 @@ const countOnly = function(allItems, itemsToCount) {
   return results; // return the object that now stores the values
 };
 
-// Test
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const results = {
-  Jason: true,
-  Karima: true,
-  Fang: true,
-  Agouhanna: false };
-
-
-const result1 = countOnly(firstNames, results);
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+module.exports = countOnly;
